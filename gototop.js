cbe41c441f9886700203bc674dd4a0a9738567d7
Tan Yan San <1211108273@student.mmu.edu.my>
2024-03-18 22:16:55 +0800

    --

---
+++ gototop.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+    let scollprogress = document.getElementById("progress");
+    let progressvalue = document.getElementById("up-icon");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollvalue = Math.round((pos * 100)/calcHeight);
+    
+    if(pos>130){
+        scollprogress.style.display = "grid";
+    }
+    else{
+        scollprogress.style.display = "none";
+    }
+    scollprogress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scollprogress.style.background = `conic-gradient(#e9b69a ${scrollvalue}%, #d7d7d7 ${scrollvalue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
---
+++ gototopbtn.php
@@ -0,0 +1,3 @@
+<div id="progress">
+    <i id="up-icon" class="fas fa-chevron-up"></i>
+</div>
